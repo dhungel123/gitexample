a6ed035b2d90eabd70a1c28844bf4d550a75fff5
ujjwal dhungel <dhungelujjwal441@gmail.com>
2024-03-01 12:09:37 +0545

    removed

--- calculator.dart
+++ calculator.dart
@@ -2,20 +2,4 @@ class Calculator {
   int add(int a, int b) {
     return a + b;
   }
-
-  int sub(int a, int b) {
-    return a - b;
-  }
-
-  int mul(int a, int b) {
-    return a * b;
-  }
-
-  int div(int a, int b) {
-    return a / b;
-  }
-
-  int power(int a) {
-    return a * a;
-  }
 }
